5fa741d1a52349abe413881380fe9749c4cffd18
Dominic DeJesus <domludejesus@gmail.com>
2020-04-07 19:25:31 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,10 +1,10 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(speak = "Squawk!",name)
+def parrot(speak = "Squawk!")
   
-  puts "  #{speak}"
+  puts "#{speak}"
   
 end 
 
-parrot("name", )
+parrot()
